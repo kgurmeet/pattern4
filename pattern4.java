9cb15957892b695e45bdf550cf87cbd8de3d1a16
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-08-20 15:26:15 +0530

    Create pattern4.java

---
+++ pattern4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class pattern4 {
+    public static void main(String[] args) {
+        int k=1,y=1;
+        for(k=1;k<=6;k++)
+        {
+            for(y=k;y<=6;y++)
+            {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
